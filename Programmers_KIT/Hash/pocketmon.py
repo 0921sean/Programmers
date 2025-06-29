1688eb19e41e9a4b57d610650e9fa6b77d95efc4
0921sean <0921sean@gmail.com>
2025-06-29 17:58:25 +0900

    [06.29]

--- Programmers_KIT/Hash/pocketmon.py
+++ Programmers_KIT/Hash/pocketmon.py
@@ -1,18 +1,14 @@
 def solution(nums):
     pocket_dict = {}
     
+    # {'포켓몬 번호': '포켓몬 개수'} 형태의 딕셔너리 생성
     for num in nums:
         pocket_dict[num] = pocket_dict.get(num, 0) + 1
     
-    pocket_num = len(pocket_dict)
-    if pocket_num >= len(nums) / 2:
-        return len(nums) / 2
-    return pocket_num
+    # 포켓몬 종류의 개수가 전체 개수의 절반보다 작거나 같으면 그 개수를 반환
+    return min(len(pocket_dict), len(nums) // 2)
 
-    # 완전 쉬운 풀이
-    # return min(len(nums) // 2, len(set(nums)))
-
-# 테스트할 케이스들
+# Test cases
 if __name__ == "__main__":
     nums_list = [[3, 1, 2, 3], [3, 3, 3, 2, 2, 4], [3, 3, 3, 2, 2, 2]]
     for nums in nums_list:
