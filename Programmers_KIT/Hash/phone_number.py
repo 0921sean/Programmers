33e5d657b87f67c21313dfe455df4d078500aacf
0921sean <0921sean@gmail.com>
2025-07-01 12:49:14 +0900

    [07.01]

--- Programmers_KIT/Hash/phone_number.py
+++ Programmers_KIT/Hash/phone_number.py
@@ -1,9 +1,11 @@
 def solution(phone_book):
     phone_dict = {}
     
+    # 전화번호부의 각 전화번호를 키로 하는 딕셔너리 생성
     for phone in phone_book:
         phone_dict[phone] = 1
         
+    # 각 전화번호의 접두사가 전화번호부에 있는지 확인
     for phone in phone_book:
         tmp = ""
         for digit in phone[:-1]:
@@ -12,7 +14,7 @@ def solution(phone_book):
                 return False
     return True
 
-# 테스트할 케이스들
+# Test cases
 if __name__ == "__main__":
     phone_book_list = [["119", "97674223", "1195524421"], ["123", "456", "789"], ["12", "123", "1235", "567", "88"]]
     for phone_book in phone_book_list:
