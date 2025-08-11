cb7a284dd87871c8f654435c70dddbc3b1ad9049
0921sean <0921sean@gmail.com>
2025-08-12 08:19:57 +0900

    [08.12]

--- Programmers_KIT/Heap/disk_controller.py
+++ Programmers_KIT/Heap/disk_controller.py
@@ -3,31 +3,30 @@ import heapq
 def solution(jobs):
     jobs.sort()
     heap = []
+    curr_time = 0   # 현재 시간
+    ret_time = 0    # 총 처리 시간
+    job_index = 0   # 현재 처리 중인 작업의 인덱스
+    job_total = len(jobs)   # 총 작업 수
     
-    current_time = 0    # 현재 시점
-    total_time = 0      # 총 반환 시간
-    job_index = 0       # jobs 배열에서 처리할 작업 인덱스
-    job_count = len(jobs)   # 총 작업 개수
-
-    while job_index < job_count or heap:
-        # 현재 시점에서 실행 가능한 작업을 heap에 추가
-        while job_index < job_count and jobs[job_index][0] <= current_time:
-            start, duration = jobs[job_index]
-            heapq.heappush(heap, (duration, start)) # (소요시간, 요청시간) 순서로 저장
+    while job_index < job_total or heap:
+        # 현재 시간에 처리할 수 있는 작업을 모두 힙에 추가
+        while job_index < job_total and jobs[job_index][0] <= curr_time:
+            req_time, cost_time = jobs[job_index]
+            heapq.heappush(heap, (cost_time, req_time)) # cost_time 기준 sort되도록
             job_index += 1
-            
-        if heap:    # 실행할 작업이 있다면
-            duration, start = heapq.heappop(heap)   # 가장 짧은 작업 선택
-            current_time += duration    # 현재 시점 업데이트
-            total_time += current_time - start  # 반환 시간 계산
-        else:   # 실행할 작업이 없다면
-            current_time = jobs[job_index][0]   # 현재 시간을 다음 작업의 요청 시간으로 업데이트
-            
-    return total_time // job_count
+        # 현재 시간에 처리할 수 있는 작업이 없다면, 다음 작업의 요청 시간이 현재 시간보다 크면 현재 시간을 갱신
+        if not heap:
+            curr_time = jobs[job_index][0]
+            continue
+        # 힙에서 가장 짧은 작업을 꺼내서 처리
+        cost_time, req_time = heapq.heappop(heap)
+        curr_time += cost_time
+        ret_time += curr_time - req_time
+    return ret_time // job_total
 
 # 테스트 케이스
 if __name__ == "__main__":
-    jobs_list = [[[0, 3], [1, 9], [2, 6]], [[0, 3], [4, 4], [5, 3], [4, 1]]]
+    jobs_list = [[[0, 3], [1, 9], [3, 5]], [[0, 3], [4, 4], [5, 3], [4, 1]]]
     for jobs in jobs_list:
         result = solution(jobs)
         print(result)
